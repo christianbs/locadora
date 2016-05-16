5d194320e12e00f8a6538fbdadd3155b7103d8bd
41304561 <41304561@087797-P33S103.macklabs.br>
2016-05-16 20:10:56 -0300

    CRUD entidade Filme.

---
+++ src/java/br/com/locadora/util/Filme.java
@@ -0,0 +1,52 @@
+
+package br.com.locadora.util;
+
+public class Filme {
+    private String titulo;
+    private String diretor;
+    private int ano;
+    private String categoria;
+    
+    public Filme (){};
+    
+    public Filme (String titulo, String diretor, int ano, String categoria){
+        this.titulo = titulo;
+        this.diretor = diretor;
+        this.ano = ano;
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+}
