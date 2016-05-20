ecf4433afbb0765243df2e91d55d27710979552e
christianbs <christian.sticchi@hotmail.com>
2016-05-20 18:57:57 -0300

    metodo do disco dao

--- src/java/br/com/locadora/negocio/DiscoNegocio.java
+++ src/java/br/com/locadora/negocio/DiscoNegocio.java
@@ -7,6 +7,13 @@ package br.com.locadora.negocio;
 
 import br.com.locadora.dados.DiscoDAO;
 import br.com.locadora.dados.impl.DiscoDAOImpl;
+import br.com.locadora.entidade.Disco;
+import br.com.locadora.entidade.Filme;
+import br.com.locadora.excessao.ExcecaoAcessoDados;
+import br.com.locadora.excessao.ExcecaoNegocio;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,6 +25,29 @@ public class DiscoNegocio {
 
     public DiscoNegocio() {
         dao = new DiscoDAOImpl();
+    }
+
+    public List<Disco> listarPorFilme(Filme filme) throws ExcecaoNegocio {
+        try {
+            return dao.listarPorFilme(filme);
+        } catch (ExcecaoAcessoDados ex) {
+            throw new ExcecaoNegocio(ex.getException());
+        }
+    }
+    
+    public static void main(String[] args) {
+        Filme filme = new Filme();
+        filme.setId(1);
+        DiscoNegocio discoNegocio = new DiscoNegocio();
+        try {
+            List<Disco> discos = discoNegocio.listarPorFilme(filme);
+            discos.stream().forEach((disco) -> {
+                System.out.println(disco.getId());
+            });
+            
+        } catch (ExcecaoNegocio ex) {
+            ex.getException().printStackTrace();
+        }
     }
 
 }
